8d5eddd24be6ad1594be61fca18c63054c9ec07b
ArisaOda <s1f101802375@iniad.org>
2021-06-18 17:40:20 +0900

    色々追加　
    リンク変更、移動等

---
+++ js/oda.js
@@ -0,0 +1,9 @@
+function btnpic(){
+    const picture = docment.getElementById("picture");
+
+    if(picture.style.display=="block"){
+        picture.style.display="none";
+    }else{
+        picture.style.display="block";
+    }
+}
