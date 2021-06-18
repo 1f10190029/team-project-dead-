b5686e5e5d26b41b6940dcece5b9afa89c00f616
ArisaOda <s1f101802375@iniad.org>
2021-06-18 17:52:17 +0900

    一旦修正

--- js/oda.js
+++ js/oda.js
@@ -1,9 +1,1 @@
-function btnpic(){
-    const picture = docment.getElementById("picture");
 
-    if(picture.style.display=="block"){
-        picture.style.display="none";
-    }else{
-        picture.style.display="block";
-    }
-}
